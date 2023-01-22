b31bbc741f82038cc8b2f79f71d0746f988cb1ca
silentshaikh <sheikhabdulmoiz283@gmail.com>
2023-01-22 23:19:31 +0500

    ubdate file

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
 let string = "";
-let buttons = document.querySelectorAll('.btn');
+let buttons = document.querySelectorAll(".btn");
 Array.from(buttons).forEach((button)=>{
-    button.addEventListener('click', (e) =>{
-        if(e.target.innerHTML == '='){
+    button.addEventListener("click",(e)=>{
+        if(e.target.innerHTML === "="){
             string = eval(string);
-            document.querySelector('input').value=string;
+            document.querySelector("input").value = string;
         }
-        else if(e.target.innerHTML == 'AC'){
+        else if(e.target.innerHTML == "C"){
             string = "";
-            document.querySelector('input').value=string;
+            document.querySelector("input").value=string;
         }
         else {
             console = (e.target)
             string = string + e.target.innerHTML;
-            document.querySelector('input').value=string;
+            document.querySelector("input").value=string;
         }
     })
 })
